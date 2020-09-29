1dc31202320c51d4ef452b3fcb87db16d5dfdc5e
Aleksandr <Serov_ae@list.ru>
2020-09-29 19:05:31 +0300

    Добавил проект по курсу "Современные средства iOS-разработчика"

---
+++ GB_Map_SA/GB_Map_SA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GB_Map_SA
+//
+//  Created by Aleksandr Serov on 28.09.2020.
+//  Copyright © 2020 Aleksandr Serov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GB_Map_SA/Pods/Target Support Files/Pods-GB_Map_SA/Pods-GB_Map_SA-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GB_Map_SA : NSObject
+@end
+@implementation PodsDummy_Pods_GB_Map_SA
+@end
